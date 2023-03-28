e7d6c722e2142fe078899aad1ac98fdbbdc179f0
Ринат <ravilov-rinat@mail.ru>
2023-03-28 09:49:04 +0300

    Initial commit

---
+++ MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileApp.swift
+//  Mobile
+//
+//  Created by Ринат on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
